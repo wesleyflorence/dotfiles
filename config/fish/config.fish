bfcd7eacab96b97a9e388634402cf7f18060ce52
Wesley Florence <jw37g@mac.com>
2026-01-31 20:55:40 -0800

    github dark theme

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,24 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
   set fish_greeting "🌊🌊🌊"
 
+  # GitHub Dark Colors (from projekt0n/github-nvim-theme)
+  set -U fish_color_normal e6edf3        # fg.default
+  set -U fish_color_command 79c0ff       # prettylights constant (bright blue)
+  set -U fish_color_keyword ff7b72       # prettylights keyword (red)
+  set -U fish_color_quote a5d6ff         # prettylights string (light blue)
+  set -U fish_color_error f85149         # danger.fg
+  set -U fish_color_param ffa657         # prettylights variable (orange)
+  set -U fish_color_comment 8b949e       # prettylights comment
+  set -U fish_color_autosuggestion 6e7681 # fg.subtle
+  set -U fish_color_cwd 3fb950           # ansi green
+  set -U fish_pager_color_prefix 79c0ff  # bright blue
+  set -U fish_pager_color_completion 848d97 # fg.muted
+  set -U fish_pager_color_description d29922 # ansi yellow
+
+  # Hydro prompt colors
+  set -U hydro_color_pwd e6edf3          # white (fg.default)
+  set -U hydro_color_git d2a8ff          # light purple
+
   # Editor
   alias vim 'nvim'
   alias vi 'nvim'
@@ -33,7 +51,7 @@ if status is-interactive
   abbr ghcs 'gh copilot suggest'
   abbr ghce 'gh copilot explain'
 
-  # ls
+  # ls (eza)
   abbr ls 'eza'
   abbr ll 'eza -al'
   abbr llt 'eza -aT'
--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,32 +1,31 @@
--- return {
---   "projekt0n/github-nvim-theme",
---   config = function()
---     require('github-theme').setup({
---       options = {
---         theme_style = "dark_default",
---         transparent = true,
---         sidebars = {"qf", "vista_kind", "terminal", "packer"},
---       }
---     })
---     vim.cmd('colorscheme github_dark')
---   end,
---   priority = 1000
--- }
-
 return {
-  "ellisonleao/gruvbox.nvim",
+  "projekt0n/github-nvim-theme",
   priority = 1000,
-  config = function ()
-    local gruvbox = require 'gruvbox'
-      gruvbox.setup {
-        transparent_mode = true,
-        -- palette_overrides = {
-        --   bright_green = "#fe8019",
-        --   bright_orange = "#b8bb26"
-        -- }
-    }
-
-    vim.o.background = "dark"
-    vim.cmd([[colorscheme gruvbox]])
-  end
+  config = function()
+    require('github-theme').setup({
+      options = {
+        transparent = true,
+      }
+    })
+    vim.cmd('colorscheme github_dark')
+  end,
 }
+
+-- Gruvbox (commented out)
+-- return {
+--   "ellisonleao/gruvbox.nvim",
+--   priority = 1000,
+--   config = function ()
+--     local gruvbox = require 'gruvbox'
+--       gruvbox.setup {
+--         transparent_mode = true,
+--         -- palette_overrides = {
+--         --   bright_green = "#fe8019",
+--         --   bright_orange = "#b8bb26"
+--         -- }
+--     }
+--
+--     vim.o.background = "dark"
+--     vim.cmd([[colorscheme gruvbox]])
+--   end
+-- }
